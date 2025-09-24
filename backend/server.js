3db5864cf98b634b3db80a4c9d01b556c700ee8a
Dilpreet Sandhu <dappysandhu1@gmail.com>
2025-09-24 14:32:05 -0700

    Express Backend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("CERA Backend Running"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log("Server running on port 5000")))
+  .catch(err => console.log(err));
