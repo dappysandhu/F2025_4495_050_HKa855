090079363fd89ad1c290fb79fa386db7e68b98b0
Dilpreet Sandhu <dappysandhu1@gmail.com>
2025-09-26 20:39:30 +0000

    fixed minnor bugs to merge develop with main branch

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-const User = require("./models/User");
+import "dotenv/config";
+import express, { json } from "express";
+import { connect } from "mongoose";
+import cors from "cors";
+import User from "./models/User.js";
+import "dotenv/config";
 
 const app = express();
 app.use(cors());
 
 // middleware
-app.use(express.json());
+app.use(json());
 
 // health check route
 app.get("/", (req, res) => res.send("CERA Backend Running"));
-
-
 
 // test route to create a user
 app.get("/test-user", async (req, res) => {
@@ -22,20 +21,16 @@ app.get("/test-user", async (req, res) => {
   res.json(user);
 });
 
-
-
-
-
 // connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-    .then(() => {
-        console.log("--------------------------------");
-        console.log("MongoDB connected successfully");
-        app.listen(5000, () => console.log("Server running on port 5000"));
-    })
-    .catch(err => {
-        console.error("MongoDB connection failed:", err.message);
-    });
+  .then(() => {
+    console.log("--------------------------------");
+    console.log("MongoDB connected successfully");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err.message);
+  });
