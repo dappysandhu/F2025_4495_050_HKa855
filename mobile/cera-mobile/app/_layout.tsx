56a37f79bc54eae5edb69ea70c22167a943fe3bc
Dilpreet Sandhu <dappysandhu1@gmail.com>
2025-10-09 00:33:14 -0700

    hot fix app stating

--- mobile/cera-mobile/app/_layout.tsx
+++ mobile/cera-mobile/app/_layout.tsx
@@ -1,11 +1,6 @@
 import { Stack } from 'expo-router';
 import React from 'react';
 
-export default function Layout() {
-  return (
-    <Stack
-      initialRouteName="auth/login" 
-      screenOptions={{ headerShown: false }}
-    />
-  );
+export default function RootLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
